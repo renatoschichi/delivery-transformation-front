7afcc4950697a4d76951db125931212ac6f2b8d5
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-09 15:37:55 -0300

    menu screen first steps

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,21 @@
+import { MenuCard } from "../components/MenuCard/MenuCard";
+import { Navbar } from "../components/Navbar/Navbar";
+import '../../src/styles/menu.scss';
+import { red } from "@mui/material/colors";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 export function Menu() {
     return (
         <div>
-            SCREEN MENU
+            <Navbar />
+            <div className="first-line">
+                <h3>Linha Supremo</h3>
+                <ShoppingCartIcon fontSize="small" sx={{ color: red[500] }} />
+            </div>
+            <MenuCard />
+            <MenuCard />
+            <button className="add-itens-btn">Adicionar mais itens</button>
+            <button type="button" id="checkout-button" className="checkout-button">Finalizar Pedido</button>
         </div>
     )
 }
