9bef66035c916503f166be50ffe4c155904fdd1c
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-14 15:15:46 -0300

    feat: first steps with swiper

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,26 +1,22 @@
 import mrshake1 from '../../assets/images/mrshake1.jpg';
 import mrshake2 from '../../assets/images/mrshake2.jpg';
 import mrshake3 from '../../assets/images/mrshake3.jpg';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import mrshake4 from '../../assets/images/mrshake4.jpg';
+import mrshake5 from '../../assets/images/mrshake5.jpg';
+import mrshake6 from '../../assets/images/mrshake6.jpg';
 import '../../styles/carousel.scss';
-
-function handleTransition() {
-    console.log('testing the function');
-}
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export function Carousel() {
     return (
-        <div className="carousel">
-            <div className="arrow" onClick={handleTransition}>
-                <ArrowBackIosIcon />
-            </div>
-            <img src={mrshake1} />
-            <img src={mrshake2} />
-            <img src={mrshake3} />
-            <div className="arrow" onClick={handleTransition}>
-                <ArrowForwardIosIcon />
-            </div>
-        </div>
+        <Swiper spaceBetween={10} slidesPerView={3}>
+            <SwiperSlide><img src={mrshake1} /></SwiperSlide>
+            <SwiperSlide><img src={mrshake2} /></SwiperSlide>
+            <SwiperSlide><img src={mrshake3} /></SwiperSlide>
+            <SwiperSlide><img src={mrshake4} /></SwiperSlide>
+            <SwiperSlide><img src={mrshake5} /></SwiperSlide>
+            <SwiperSlide><img src={mrshake6} /></SwiperSlide>
+        </Swiper>
     )
 }
