5ea5819fc5f64051c11570ab9c258bf8570d8cb4
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-14 13:58:36 -0300

    feat: carousel first steps

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,30 +1,26 @@
-import { Box, Grid } from '@mui/material';
+import mrshake1 from '../../assets/images/mrshake1.jpg';
+import mrshake2 from '../../assets/images/mrshake2.jpg';
+import mrshake3 from '../../assets/images/mrshake3.jpg';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import '../../styles/carousel.scss';
+
+function handleTransition() {
+    console.log('testing the function');
+}
 
 export function Carousel() {
     return (
-        <div>
-            <Box>
-    <Grid container spacing={6}>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-        <Grid item xs={2}>
-            <img />
-        </Grid>
-                </Grid>
-            </Box>
+        <div className="carousel">
+            <div className="arrow" onClick={handleTransition}>
+                <ArrowBackIosIcon />
+            </div>
+            <img src={mrshake1} />
+            <img src={mrshake2} />
+            <img src={mrshake3} />
+            <div className="arrow" onClick={handleTransition}>
+                <ArrowForwardIosIcon />
+            </div>
         </div>
     )
 }
