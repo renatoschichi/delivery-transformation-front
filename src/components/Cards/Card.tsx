5428a53e6c9dd6b9b5b816368858ec7eed21076f
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-19 13:59:14 -0300

    feat: new logic in checkout screen about edit button

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,51 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
+const initialAddresses = [
+    {
+        title: "João Ribeiro",
+        street: "Avenida senhor do Bonfim, 339, Centro - Caprim Grosso",
+        complement: "Apto, 1º Andar",
+        reference: "Ao lado da Center Móveis"
+    },
+    {
+        title: "João da silva",
+        street: "Rua tupiniqui ",
+        complement: "Apto, 2º Andar",
+        reference: "Ao lado do cemitério"     
+    }
+]
+
 export function Card() {
+    const [ addresses, setAddresses ] = useState(initialAddresses)
+    const [ selectedAddress, setSelectedAddress ] = useState<number | null>(null);
+
     return (
         <React.Fragment>
+            {addresses.map((address, index: number) => (
+                <div className="card" key={index}>
+                    <Box>
+                        <Grid container spacing={2}>
+                            <Grid item xs={10}>
+                            <h3>{address.title}</h3>
+                            </Grid>
+                            <Grid item xs={2}>
+                            <div className="checkout-checkbox">
+                                <input type="radio" id="first-address" name="checkbox" value="first-address" onClick={() => setSelectedAddress(index)} />
+                            </div>
+                            </Grid>
+                        </Grid>
+                    </Box>
 
-            <div className="card">
-            <Box>
-                <Grid container spacing={2}>
-                    <Grid item xs={10}>
-                    <h3>João Ribeiro</h3>
-                    </Grid>
-                    <Grid item xs={2}>
-                    <div className="checkout-checkbox">
-                        <input type="radio" id="first-address" name="checkbox" value="first-address" />
-                    </div>
-                    </Grid>
-                </Grid>
-            </Box>
-
-                <span>Avenida senhor do Bonfim, 339, Centro - Caprim Grosso</span>
-                <span>Complemento: Apto, 1º Andar</span>
-                <span>Ponto de referência: Ao lado da Center Móveis</span>
-                <p className="edit-button">Editar</p>
-            </div>
-
-            <div className="card">
-            <Box>
-                <Grid container spacing={2}>
-                    <Grid item xs={10}>
-                    <h3>João Ribeiro</h3>
-                    </Grid>
-                    <Grid item xs={2}>
-                    <div className="checkout-checkbox">
-                        <input type="radio" id="second-address" name="checkbox" value="second-address" />
-                    </div>
-                    </Grid>
-                </Grid>
-            </Box>
-
-                <span>Avenida senhor do Bonfim, 339, Centro - Caprim Grosso</span>
-                <span>Complemento: Apto, 1º Andar</span>
-                <span>Ponto de referência: Ao lado da Center Móveis</span>
-                <p className="edit-button">Editar</p>
-            </div>
-
+                    <span>{address.street}</span>
+                    <span>Complemento: {address.complement}</span>
+                    <span>Ponto de referência: {address.reference}</span>
+                    {index === selectedAddress && (
+                        <p className="edit-button" id="edit-button-1">Editar</p>
+                    )}
+                </div>
+            ))}
         </React.Fragment>
     );
 }
