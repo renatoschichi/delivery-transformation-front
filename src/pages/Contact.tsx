3f5f8ab22a20bb6ccd4c98b010329761a148db17
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-19 14:24:09 -0300

    feat: adding navbar in all screens

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from "../components/Navbar/Navbar"
+
 export function Contact() {
     return (
         <div>
+            <Navbar />
             <h1>CONTACT SCREEN</h1>
         </div>
     )
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from "../components/Navbar/Navbar"
+
 export function User() {
     return (
         <div>
+            <Navbar />
             <h1>USER SCREEN</h1>
         </div>
     )
