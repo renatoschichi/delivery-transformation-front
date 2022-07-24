19b6df2deeda1a2a6709e2f6b83bd885e016428b
Renato Fonseca <renato.fonseca@partner.sdslasupport.com>
2022-07-23 23:45:15 -0300

    new rules in checkout first steps

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,10 +1,13 @@
 import '../styles/checkout.scss';
 import '../styles/button.scss';
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../components/Cards/Card";
-import { PaymentType } from "../components/PaymentType/PaymentType";
+import { FormGroup } from "@mui/material";
 
 export function Checkout() {
+
+    const [ selectedPaymentType, setSelectedPaymentType ] = useState<any>(null);
+
     return (
         <div className="container">
             <h1>
@@ -14,13 +17,34 @@ export function Checkout() {
             <div className="card-container">
                 <h2 className="checkout-h1">Local de entrega</h2>
                 <Card />
-                <PaymentType />
+
+                <div>
+                    <h3 className="checkout-h1">Formas de Pagamento</h3>
+                    <FormGroup>
+                    <div className="payment-type">
+                        <input type="radio" id="money-payment" name="payment-type" value="money-payment" />
+                        <label>Dinheiro</label>
+                    </div>
+                    <div className="payment-type">
+                        <input type="radio" id="debit-card-payment" name="payment-type" value="debit-card-payment" />
+                        <label>Cartão de Débito</label>
+                    </div>
+                    <div className="payment-type">
+                        <input type="radio" id="credit-card-payment" name="payment-type" value="credit-card-payment" />
+                        <label>Cartão de Crédito</label>
+                    </div>
+                    </FormGroup>
+                </div>
+
             </div>
 
             <h2 className="checkout-h1">Troco para quanto?</h2>
 
             <div className="troco">
-                <p>R$ 40,00</p>
+                <div className="troco-1">
+                    <p>R$ 40,00</p>
+                    <input type="radio" id="troco" name="troco" value="troco" className="input-checkout-camp" />
+                </div>
                 <hr/>
                 <span>Não vai precisar de troco</span>
                 <input type="radio" id="troco" name="troco" value="troco" />
